a5129d132205f203ee840aff8bbe3963ebefe372
gem007bd <gem007bd@yahoo.com>
2019-05-09 16:12:17 +0200

    add image to the post

--- index.js
+++ index.js
@@ -1,36 +1,105 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const expressEdge = require('express-edge');
+const bodyParser = require('body-parser');
+const path = require('path');
+const fileUpload = require('express-fileupload');
+
+
+/**
+ *  Call the Post Model
+ */
+const Post = require('./database/models/Post');
+
 const app = express();
 
+/**
+ * connect with the database (node-js-blog)
+ */
 mongoose.connect('mongodb://localhost/node-js-blog');
-//setting middleware
-app.use(express.static('public'));
 
-// require express-edge
+/**
+ * Use fileupload for uploading file
+ */
+app.use(fileUpload());
+
+/**
+ * body parser to parse the request
+ * parse application/x-www-form-urlencoded
+ */
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json())
+
+
+/**
+ *  Use the template engine 
+ */
+app.use(express.static('public'));
 app.use(expressEdge);
 app.set('views', `${__dirname}/views/layouts`);
 
-app.get('/', (req, res, next) => {
-    res.render('index');
+/**
+ * get all the post in the index page
+ */
+app.get('/', async(req, res) => {
+
+    const posts = await Post.find({});
+    console.log(posts);
+    res.render('index', {
+        posts
+    });
 });
 
+/**
+ * Get data from about page
+ */
 app.get('/about', (req, res, next) => {
     res.render('about');
 });
 
-app.get('/post', (req, res, next) => {
-    res.render('post');
+// app.get('/post', (req, res, next) => {
+//     res.render('post');
+// });
+
+/**
+ * request for a post with id
+ */
+app.get('/posts/:id', async(req, res, next) => {
+    const singlePost = await Post.findById(req.params.id);
+    res.render('post', {
+        singlePost
+    });
 });
 
 app.get('/contact', (req, res, next) => {
     res.render('contact')
 });
 
-// url for create a post 
-app.get('/posts/new', (req, res, next) => {
+/**
+ * Create new post page
+ */
+app.get('/post/create', (req, res, next) => {
     res.render('create')
 });
+
+/**
+ * Post the request to the url to create Post
+ */
+app.post('/posts/store', (req, res, next) => {
+    const image = req.files.post_image;
+    // upload the file to the post 
+    image.mv(path.resolve(__dirname, 'public/posts', image.name), (error) => {
+        Post.create({
+            ...req.body,
+            image: `/posts/${image.name}`
+        }, (error, post) => {
+            res.redirect('/');
+        });
+    });
+})
+
 // admin login panel
 app.get('/admin', (req, res, next) => {
     res.render('admin')
--- test.js
+++ test.js
@@ -4,13 +4,14 @@ const Post = require('./database/models/Post');
 
 mongoose.connect('mongodb://localhost/node-js-blog');
 
-// Post.create({
-//     title: 'My second blog post',
-//     description: 'second Blog post description',
-//     content: 'second lorem ipsom for the blog'
-// }, (error, post) => {
-//     console.log(error, post)
-// })
+Post.create({
+    title: 'My second blog post',
+    username: 'test',
+    description: 'second Blog post description',
+    content: 'second lorem ipsom for the blog'
+}, (error, post) => {
+    console.log(error, post)
+})
 
 // Post.find({
 //     title: 'My second blog post'
@@ -22,8 +23,8 @@ mongoose.connect('mongodb://localhost/node-js-blog');
 //     console.log(error, post);
 // })
 
-Post.findByIdAndUpdate('5cd0431a72ed7735c88b91c8', {
-    title: 'My updated blog post title'
-}, (error, post) => {
-    console.log(error, post);
-});
+// Post.findByIdAndUpdate('5cd0431a72ed7735c88b91c8', {
+//     title: 'My updated blog post title'
+// }, (error, post) => {
+//     console.log(error, post);
+// });
